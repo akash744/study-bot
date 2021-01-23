8524d694fe725d66e51c5dfdb79a64abf1c552fc
NivranshuB <nbos433@aucklanduni.ac.nz>
2021-01-23 22:41:29 +1300

    Initial implementation for the weekly timetable

---
+++ my-app/src/timetable-components/timetable_page.js
@@ -0,0 +1,136 @@
+  
+import React from 'react';
+import './timetable.css';
+
+const TimetablePage = () => {
+    return (
+        <div>
+            <h1 class="logo">
+                Timetable
+            </h1>
+            <div class="grid">
+                <div class="day monday">Monday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+                <div class="day tuesday">Tuesday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+                <div class="day wednesday">Wednesday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+                <div class="day thursday">Thursday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+                <div class="day friday">Friday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+                <div class="day saturday">Saturday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+                <div class="day sunday">Sunday
+                    <div>
+                        <div class="hour">07:00 - 08:00</div>
+                        <div class="hour">08:00 - 09:00</div>
+                        <div class="hour">09:00 - 10:00</div>
+                        <div class="hour">10:00 - 11:00</div>
+                        <div class="hour">11:00 - 12:00</div>
+                        <div class="hour">12:00 - 13:00</div>
+                        <div class="hour">13:00 - 14:00</div>
+                        <div class="hour">14:00 - 15:00</div>
+                        <div class="hour">15:00 - 16:00</div>
+                        <div class="hour">16:00 - 17:00</div>
+                        <div class="hour">17:00 - 18:00</div>
+                        <div class="hour">18:00 - 19:00</div>
+                        <div class="hour">19:00 - 20:00</div>
+                    </div>
+                </div>
+            </div>   
+        </div>
+    )
+}
+
+export default TimetablePage;
