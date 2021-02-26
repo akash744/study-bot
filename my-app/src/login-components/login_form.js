271953f6a7f13ba587ff938dddb39c52a971d858
akash744 <akashashok12341999@gmail.com>
2021-02-26 23:32:07 +1300

    updated styling added google auth

--- my-app/src/login-components/login_form.js
+++ my-app/src/login-components/login_form.js
@@ -1,35 +1,58 @@
-import React from 'react';  
+import React, { Component } from 'react';  
+import GoogleLogin from 'react-google-login';
 
 
-function LoginForm(){
+class LoginForm extends Component{
 
+	responseGoogle=(response)=>{
+		console.log(response);
+		console.log(response.profileObj);
+	  }
 
-    return(
-		<div class="cont">
-			<div class="form sign-in">
-				<h2>Sign In</h2>
-      			<label>
-				  <span>Email Address</span>
-				  <input type="email" name="email"></input>
-				</label>
-				<label>
-					<span>Password</span>
-					<input type="password" name="password"></input>
-      			</label>
-				<button class="submit" type="button">Sign In</button>
-				<p class="forgot-pass">Forgot Password ?</p>
-
-			</div>	
-			<div class="sub-cont">
-				<div class="img">
-					<div class="img-text m-up">
-						<h2>New here?</h2>
-          				<p>Sign up and discover great amount of new opportunities!</p>
+	render(){
+		return(
+			<div class="cont">
+				<div class="form sign-in">
+					<h1 id='logo'>
+						Study-Bot
+					</h1>
+					<h2 id='slogan-line-1'>Artificial Intelligence Driving</h2>
+					<h2 id='slogan-line-2'>Results for Studying</h2>
+					<h3 id= 'subtitle'>Welcome Please Sign In Or Sign Up.</h3>
+					  <label>
+					  <input type="email" placeholder="Email" ></input>
+					</label>
+					<label>
+						<input type="password" placeholder="Password" ></input>
+					  </label>
+					<div id="sign-in-sign-up">
+						  <button class="submit" type="button">Sign In</button>
+						<button class="submit" type="button">Sign Up</button>
+					</div>
+					<div id="google-login">
+						<h5>Or Sign In With</h5>	
+						<GoogleLogin
+						clientId="631622997506-c5cgjg1hl5nqe8mdjlhqa05qiofo27d8.apps.googleusercontent.com"
+						buttonText="Login"
+						onSuccess={this.responseGoogle}
+						onFailure={this.responseGoogle}
+						cookiePolicy={'single_host_origin'}
+	
+						/>
+					</div>
+	
+				</div>	
+				<div class="sub-cont">
+					<div class="img">
+						<div class="img-text m-up">
+							<h2>New here?</h2>
+							  <p>Sign up and discover great amount of new opportunities!</p>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>	
-    )      
+			</div>	
+		)
+	}  
 }
 
 export default LoginForm;
